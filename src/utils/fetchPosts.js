0b133ecfa467d3c8e52e7d63980a4e8e58e8b24d
Brian Wilson <bmwilson93@gmail.com>
2024-07-24 04:27:02 -0600

    moved the url builder to it's own module

--- src/utils/fetchPosts.js
+++ src/utils/fetchPosts.js
@@ -1,13 +1,8 @@
+import urlBuilder from "./urlBuilder";
+
 const fetchPosts = async (search = "", category = "") => {
-  // build out the url for the request with the search and category
-  // let url = 'https://www.reddit.com/r/pokemon/.json';
-
-  let url = 'https://www.reddit.com/';
-  if (category.length > 0) url += `r/${category}/`;
-  url += '.json';
-  if (search.length > 0) url += `>?q=${search}`;
-
-
+  let url = urlBuilder(search, category);
+  
   // make the request in a try/catch
   try {
     let result = await fetch(url);
@@ -16,9 +11,6 @@ const fetchPosts = async (search = "", category = "") => {
     console.log(data)
 
     return(data.data.children)
-    // return [];
-    // TESTING
-    // return [{name: 'steve', fun: yes}, {name: rick, fun: no}];
   } catch (err) {
     console.log(" error and stuff! " + err);
     return [];
