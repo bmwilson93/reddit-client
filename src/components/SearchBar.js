5e648d89502afce16c6de059051f390726a65c9b
Brian Wilson <bmwilson93@gmail.com>
2024-07-26 23:51:26 -0600

    created SearchBar.js

---
+++ src/components/SearchBar.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Button from 'react-bootstrap/Button';
+
+// Redux Imports 
+import { useSelector, useDispatch } from 'react-redux' // allows to interact with the store
+import { updateSearch } from '../slices/postSlice'; // the refucer function from the slice
+
+const SearchBar = ({ getData }) => {
+  const currentSearch = useSelector(state => state.post.search);
+  const dispatch = useDispatch() // binds the useDispatch function to just dispatch
+
+  return (
+    <div>
+      <form>
+        <input type="text"
+          value={currentSearch}
+          onChange={(e) => dispatch(updateSearch(e.target.value))}
+        />
+        <Button variant="success" onClick={getData}>Search Posts</Button>
+      </form>
+      <p>{currentSearch}</p>
+    </div>
+  )
+}
+
+export default SearchBar
