1dd0b982ca38ce54fb7dcbb73f941071bbe84951
Brian Wilson <bmwilson93@gmail.com>
2024-07-26 23:49:53 -0600

    added a search state to the slice

--- src/slices/postSlice.js
+++ src/slices/postSlice.js
@@ -3,15 +3,23 @@ import { createSlice } from '@reduxjs/toolkit';
 export const postSlice = createSlice({
   name: 'post',
   initialState: {
-    posts: []
+    posts: [],
+    category: "",
+    search: ""
   },
   reducers: {
     // update Posts, takes an action, which should hold an array of posts, and updates the state to equal that array
     updatePosts: (state, action ) => {
       state.posts = action.payload
+    },
+    updateCategory: (state, action)  => {
+      state.category = action.payload
+    },
+    updateSearch: (state, action) => {
+      state.search = action.payload
     }
   }
 });
 
-export const { updatePosts } = postSlice.actions;
+export const { updatePosts, updateCategory, updateSearch } = postSlice.actions;
 export default postSlice.reducer;
