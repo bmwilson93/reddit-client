4e4f7e929aae33d780d87c559e8624585f55b868
Brian Wilson <bmwilson93@gmail.com>
2024-07-26 23:44:06 -0600

    moved the function for calling fetchPosts to it's own definition

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { useEffect } from "react";
 import './App.css';
+import SearchBar from "./components/SearchBar.js";
+import Categories from "./components/Categories.js";
 
 // Post Stuff Imports
 import PostList from './components/PostList.js';
@@ -10,22 +12,31 @@ import { useSelector, useDispatch } from 'react-redux' // allows to interact wit
 import { updatePosts } from './slices/postSlice'; // the refucer function from the slice
 
 // !!! Add mock data to the store for now !!!
-import mockData from './utils/mockData.js'
+// import mockData from './utils/mockData.js'
 
 
 function App() {
   const dispatch = useDispatch() // binds the useDispatch function to just dispatch
+  const currentSearch = useSelector(state => state.post.search);
+  const currentCategory = useSelector(state => state.post.category);
+
+  // function that retrives the posts and updates them in the redux store
+  const getData = async () => {
+    const data = await fetchPosts(currentSearch, currentCategory);
+    dispatch(updatePosts(data));
+  }
 
   // get the reddit posts data with useEffect on load, save it to redux store
   // !!! currently getting from mockData, change to fetchPosts util after testing !!!
   useEffect(() => {
-    dispatch(updatePosts(mockData));
+    getData()
   }, [])
   
 
   return (
     <div className="App">
-
+      <Categories />
+      <SearchBar getData={getData}/>
       <PostList />
 
     </div>
